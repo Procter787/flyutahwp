1b01caa567be5d4e3cd53dd0350e9b7ed855fe6e
Fin Morrison <fin.morrison@gmail.com>
2021-06-23 08:49:34 -0600

    removing unused appearance menu sections

--- inc/site-settings.php
+++ inc/site-settings.php
@@ -122,4 +122,20 @@ function my_login_logo() {
 add_action( 'login_enqueue_scripts', 'my_login_logo' );
 
 //Add other Site Settings here
+
+//Change appearance->customize menu in CMS to hide un-used elements
+function epicpress_remove_sections( $wp_customize ) {
+
+	// $wp_customize->remove_section('header_image');
+	// $wp_customize->remove_panel('nav_menus');
+	//$wp_customize->remove_panel('widgets');
+	//$wp_customize->remove_section('custom_css');	
+	$wp_customize->remove_section('colors');
+	$wp_customize->remove_section('background_image');
+	//$wp_customize->remove_section('static_front_page');	 
+	// $wp_customize->remove_section('title_tagline');	
+    $wp_customize->remove_control('custom_logo');
+}
+
+add_action( 'customize_register', 'epicpress_remove_sections');
 ?>
